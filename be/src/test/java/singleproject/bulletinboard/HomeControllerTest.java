14755c8149140c3c83765a292088ea1306f620f2
tokaji <fpzk5656@gmail.com>
2022-09-15 01:34:20 +0900

    refactor HomeControllerTest를 단위테스트로 만들기 위해 쓸데없는 부분은 수정함

--- be/src/test/java/singleproject/bulletinboard/HomeControllerTest.java
+++ be/src/test/java/singleproject/bulletinboard/HomeControllerTest.java
@@ -2,24 +2,31 @@ package singleproject.bulletinboard;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import singleproject.bulletinboard.controller.HomeController;
 
-@WebMvcTest
 class HomeControllerTest {
 
-	@Autowired
+	private HomeController homeController;
+
 	MockMvc mockMvc;
+
+	@BeforeEach
+	public void setUp() {
+		homeController = new HomeController();
+		this.mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+	}
 
 	@DisplayName("홈페이지로 리다이렉트")
 	@Test
 	void redirectHomePage() throws Exception {
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/home");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
 		mockMvc.perform(requestBuilder)
 			.andExpect(status().is3xxRedirection());
 	}
