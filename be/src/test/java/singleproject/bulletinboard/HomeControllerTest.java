45e2d084d15ef94d7d5285f8e759005e53f02604
tokaji <fpzk5656@gmail.com>
2022-09-19 02:25:29 +0900

    feat 홈페이지 로직을 테스트에 추가

--- be/src/test/java/singleproject/bulletinboard/HomeControllerTest.java
+++ be/src/test/java/singleproject/bulletinboard/HomeControllerTest.java
@@ -32,4 +32,36 @@ class HomeControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(forwardedUrlTemplate("home"));
 	}
+
+	@DisplayName("홈 버튼: 홈으로 리다이렉트")
+	@Test
+	void redirectHome() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("redirect:/");
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().is3xxRedirection());
+	}
+	@DisplayName("로그인 버튼: 로그인 페이지로 이동한다.")
+	@Test
+	void goLoginPage() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/login");
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isOk())
+			.andExpect(forwardedUrlTemplate("login"));
+	}
+	@DisplayName("회원가입 버튼: 회원가입 페이지로 이동한다.")
+	@Test
+	void goRegisterPage() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/register");
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isOk())
+			.andExpect(forwardedUrlTemplate("register"));
+	}
+	@DisplayName("게시판 버튼: 게시판 페이지로 이동한다.")
+	@Test
+	void goArticlesPage() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/board");
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isOk())
+			.andExpect(forwardedUrlTemplate("board"));
+	}
 }
