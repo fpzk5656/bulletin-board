05d9c412e56fa6d6402a40b9d8000de3c726c055
tokaji <fpzk5656@gmail.com>
2022-09-20 23:40:44 +0900

    feat 홈페이지 로직을 HomeController에 추가

--- be/src/main/java/singleproject/bulletinboard/controller/HomeController.java
+++ be/src/main/java/singleproject/bulletinboard/controller/HomeController.java
@@ -10,4 +10,19 @@ public class HomeController {
 	public String home() {
 		return "home";
 	}
+
+	@GetMapping("/login")
+	public String goLogin() {
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String goRegister() {
+		return "register";
+	}
+
+	@GetMapping("/board")
+	public String goBoard() {
+		return "board";
+	}
 }
