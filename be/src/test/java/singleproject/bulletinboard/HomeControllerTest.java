d582004b18f43ae541286a6a83f933aef9a37d30
tokaji <fpzk5656@gmail.com>
2022-09-27 18:57:26 +0900

    refactor 홈페이지 로직 테스트 방식을 변경

--- be/src/test/java/singleproject/bulletinboard/HomeControllerTest.java
+++ be/src/test/java/singleproject/bulletinboard/HomeControllerTest.java
@@ -1,14 +1,13 @@
 package singleproject.bulletinboard;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrlTemplate;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import singleproject.bulletinboard.controller.HomeController;
 
@@ -20,48 +19,41 @@ class HomeControllerTest {
 
 	@BeforeEach
 	public void setUp() {
-		homeController = new HomeController();
+		this.homeController = new HomeController();
 		this.mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
 	}
 
 	@DisplayName("접속 시 홈페이지로 이동")
 	@Test
 	void goHomePage() throws Exception {
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
-		mockMvc.perform(requestBuilder)
+		mockMvc.perform(get("/"))
 			.andExpect(status().isOk())
-			.andExpect(forwardedUrlTemplate("home"));
+			.andExpect(forwardedUrl("home"));
 	}
 
-	@DisplayName("홈 버튼: 홈으로 리다이렉트")
-	@Test
-	void redirectHome() throws Exception{
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("redirect:/");
-		mockMvc.perform(requestBuilder)
-			.andExpect(status().is3xxRedirection());
-	}
 	@DisplayName("로그인 버튼: 로그인 페이지로 이동한다.")
 	@Test
-	void goLoginPage() throws Exception{
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/login");
-		mockMvc.perform(requestBuilder)
+	void goLoginPage() throws Exception {
+
+		mockMvc.perform(get("/login"))
 			.andExpect(status().isOk())
-			.andExpect(forwardedUrlTemplate("login"));
+			.andExpect(forwardedUrl("page/login"));
 	}
+
 	@DisplayName("회원가입 버튼: 회원가입 페이지로 이동한다.")
 	@Test
-	void goRegisterPage() throws Exception{
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/register");
-		mockMvc.perform(requestBuilder)
+	void goRegisterPage() throws Exception {
+
+		mockMvc.perform(get("/register"))
 			.andExpect(status().isOk())
-			.andExpect(forwardedUrlTemplate("register"));
+			.andExpect(forwardedUrl("page/register"));
 	}
+
 	@DisplayName("게시판 버튼: 게시판 페이지로 이동한다.")
 	@Test
-	void goArticlesPage() throws Exception{
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/board");
-		mockMvc.perform(requestBuilder)
+	void goBoardPage() throws Exception {
+		mockMvc.perform(get("/board"))
 			.andExpect(status().isOk())
-			.andExpect(forwardedUrlTemplate("board"));
+			.andExpect(forwardedUrl("page/login"));
 	}
 }
