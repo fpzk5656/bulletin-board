07ab698ef50d0fe87a36bb05230f5edc0fe37291
tokaji <fpzk5656@gmail.com>
2022-10-07 23:29:33 +0900

    feat 회원 도메인 클래스 추가

--- be/src/main/java/singleproject/bulletinboard/domain/Member.java
+++ be/src/main/java/singleproject/bulletinboard/domain/Member.java
@@ -5,8 +5,10 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 public class Member {
