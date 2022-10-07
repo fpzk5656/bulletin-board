467307e3335e43bc7f89e3cde7dd4ecd88f14b76
tokaji <fpzk5656@gmail.com>
2022-10-07 23:29:53 +0900

    feat 회원 레포지토리 추가

---
+++ be/src/main/java/singleproject/bulletinboard/repository/MemoryMemberRepository.java
@@ -0,0 +1,56 @@
+package singleproject.bulletinboard.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import singleproject.bulletinboard.domain.Member;
+
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
+
+	private final ConcurrentHashMap<Long, Member> store = new ConcurrentHashMap<>();
+	private static AtomicLong sequence = new AtomicLong();
+
+	@Override
+	public Member save(Member member) {
+
+		Long newId = sequence.incrementAndGet();
+		Member newMember = member.builder()
+			.id(newId)
+			.age(member.getAge())
+			.point(member.getPoint())
+			.name(member.getName())
+			.birthday(member.getBirthday())
+			.build();
+		store.put(newId, newMember);
+		return newMember;
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+
+		return Optional.ofNullable(store.get(id));
+	}
+
+	@Override
+	public Optional<Member> findByName(String name) {
+
+		return store.values().stream()
+			.filter(member -> member.getName().equals(name))
+			.findAny();
+	}
+
+	@Override
+	public List<Member> findAll() {
+
+		return new ArrayList<>(store.values());
+	}
+
+	public void clearStore() {
+		store.clear();
+		sequence = new AtomicLong();
+	}
+}
