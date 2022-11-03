be95220c6350770b191e1c93f9c4ddb7a5b3ac2f
tokaji <fpzk5656@gmail.com>
2022-11-03 23:58:55 +0900

    feat 유저 로그인 폼 추가

---
+++ be/src/main/java/singleproject/bulletinboard/controller/UserLoginForm.java
@@ -0,0 +1,12 @@
+package singleproject.bulletinboard.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginForm {
+
+	private String name;
+	private String password;
+}
