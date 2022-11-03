721abd10c5df0e2f63e7dd8ad2a9b5ffcec4b674
tokaji <fpzk5656@gmail.com>
2022-11-04 00:11:49 +0900

    refactor 홈 컨트롤러에 로그인 했을 때의 상황 추가

--- be/src/main/java/singleproject/bulletinboard/controller/HomeController.java
+++ be/src/main/java/singleproject/bulletinboard/controller/HomeController.java
@@ -1,15 +1,23 @@
 package singleproject.bulletinboard.controller;
 
-import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import singleproject.bulletinboard.domain.Article;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import singleproject.bulletinboard.domain.Member;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String home() {
-		return "home";
+	public String loginHome(
+		@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) Member loginUser,
+		Model model) {
+		if(loginUser == null){
+			return "home";
+		}
+
+		model.addAttribute("user", ResponseUserInfo.from(loginUser));
+		return "page/loginHome";
 	}
 }
