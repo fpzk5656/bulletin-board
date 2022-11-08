4d0b8091930f24ada11aec7c08548c0cb7d485e0
tokaji <fpzk5656@gmail.com>
2022-11-09 01:40:43 +0900

    refactor 회원가입 폼에 생일 입력 형태 설정

--- be/src/main/java/singleproject/bulletinboard/controller/RequestJoinMemberInfo.java
+++ be/src/main/java/singleproject/bulletinboard/controller/RequestJoinMemberInfo.java
@@ -1,17 +1,21 @@
 package singleproject.bulletinboard.controller;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class RequestJoinMemberInfo {
 
 	private String name;
 	private String password;
-	private int age;
-	private LocalDateTime birthday;
+	private Integer age;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate birthday;
 }
