0a1a26ef8bfd0774af6aa315366c0cdac638f362
tokaji <fpzk5656@gmail.com>
2022-11-13 00:19:19 +0900

    refactor 로그인에 성공하면 처음 요청한 URL에 이동하도록 하였다

--- be/src/main/java/singleproject/bulletinboard/controller/LoginController.java
+++ be/src/main/java/singleproject/bulletinboard/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import singleproject.bulletinboard.domain.Member;
 import singleproject.bulletinboard.service.MemberService;
 
@@ -25,9 +26,28 @@ public class LoginController {
 		return "page/login";
 	}
 
+//	@PostMapping("/login")
+//	public String login(@Valid @ModelAttribute("loginForm") UserLoginForm loginForm, BindingResult bindingResult, HttpServletRequest request
+//	,@RequestParam(defaultValue = "/") String redirectURL){
+//
+//		if(bindingResult.hasErrors()){
+//			log.info("errors = {}", bindingResult);
+//			return "page/loginForm";
+//		}
+//
+//		Member loginUser = memberService.login(loginForm.getName(),loginForm.getPassword());
+//
+//		if(loginUser == null){
+//			log.info("login Fail");
+//			bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+//		}
+//
+//	}
+
 	@PostMapping("/login")
 	public String login(@Valid @ModelAttribute("loginForm") UserLoginForm loginForm,
-		BindingResult bindingResult, HttpServletRequest request) {
+		BindingResult bindingResult, HttpServletRequest request,
+		@RequestParam(defaultValue = "/") String redirectURL) {
 		if (bindingResult.hasErrors()) {
 			log.info("errors = {}", bindingResult);
 			return "page/login";
@@ -48,7 +68,8 @@ public class LoginController {
 		// 세션에 회원 로그인 정보 보관
 		session.setAttribute(SessionConst.LOGIN_USER, loginUser);
 
-		return "redirect:/";
+		// 로그인에 성공 시 처음 요청한 URL로 이동
+		return "redirect:" + redirectURL;
 	}
 
 	@PostMapping("/logout")
