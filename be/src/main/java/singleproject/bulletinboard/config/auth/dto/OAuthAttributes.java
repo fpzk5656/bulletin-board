1c6e74cf42e211a5ec5d192461a1cfacb6911210
tokaji <fpzk5656@gmail.com>
2022-12-07 13:04:06 +0900

    feat OAuthAttributes 추가

---
+++ be/src/main/java/singleproject/bulletinboard/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,47 @@
+package singleproject.bulletinboard.config.auth.dto;
+
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+import singleproject.bulletinboard.domain.user.Role;
+import singleproject.bulletinboard.domain.user.Member;
+
+@Builder
+@Getter
+public class OAuthAttributes {
+
+	private Map<String, Object> attributes;
+	private String nameAttributeKey;
+	private String name;
+	private String email;
+	private String picture;
+
+	public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+		Map<String, Object> attributes) {
+		return ofGoogle(userNameAttributeName, attributes);
+	}
+
+	private static OAuthAttributes ofGoogle(String userNameAttributeName,
+		Map<String, Object> attributes) {
+		return OAuthAttributes.builder()
+			.name((String) attributes.get("name"))
+			.email((String) attributes.get("email"))
+			.picture((String) attributes.get("picture"))
+			.attributes(attributes)
+			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
+
+	// User 엔티티를 생성합니다.
+	// OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때입니다.
+	// 가입할 때의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용합니다.
+	// OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser클래스를 생성합니다.
+	public Member toEntity(){
+		return Member.builder()
+			.name(name)
+			.email(email)
+			.picture(picture)
+			.role(Role.GUEST)
+			.build();
+	}
+}
