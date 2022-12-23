ab939203b71814964a0c492602a9663b94814e87
tokaji <fpzk5656@gmail.com>
2022-12-23 16:34:01 +0900

    feat OAuth2 Naver 로그인 기능 추가

--- be/src/main/java/singleproject/bulletinboard/config/auth/dto/OAuthAttributes.java
+++ be/src/main/java/singleproject/bulletinboard/config/auth/dto/OAuthAttributes.java
@@ -18,6 +18,9 @@ public class OAuthAttributes {
 
 	public static OAuthAttributes of(String registrationId, String userNameAttributeName,
 		Map<String, Object> attributes) {
+		if ("naver".equals(registrationId)) {
+			return ofNaver("id", attributes);
+		}
 		return ofGoogle(userNameAttributeName, attributes);
 	}
 
@@ -32,11 +35,24 @@ public class OAuthAttributes {
 			.build();
 	}
 
+	private static OAuthAttributes ofNaver(String userNameAttributeName,
+		Map<String, Object> attributes) {
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+		return OAuthAttributes.builder()
+			.name((String) response.get("name"))
+			.email((String) response.get("email"))
+			.picture((String) response.get("profile_image"))
+			.attributes(response)
+			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
+
 	// User 엔티티를 생성합니다.
 	// OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때입니다.
 	// 가입할 때의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용합니다.
 	// OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser클래스를 생성합니다.
-	public Member toEntity(){
+	public Member toEntity() {
 		return Member.builder()
 			.name(name)
 			.email(email)
