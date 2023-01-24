b31aaef57b959a35e2445f429919889c50d03636
sunullo <fpzk5656@gmail.com>
2023-01-24 18:42:41 +0900

    style 배포 스크립트에 jar 프로세스 찾는 부분을 java로 변경

--- be/scripts/deploy.sh
+++ be/scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl bulletin-board | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl bulletin-board | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -30,7 +30,7 @@ echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 살행"
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
