d2dca9670ea2e1e1521f1d0d46e0a63d24f1fe8c
sunullo <fpzk5656@gmail.com>
2023-01-25 13:36:31 +0900

    style profile.sh 오타 수정

--- be/scripts/profile.sh
+++ be/scripts/profile.sh
@@ -2,9 +2,9 @@
 
 # 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 
-function find_idle_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"
-                            http://localhost/profile)
+function find_idle_profile()
+{
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
   if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면(즉, 40x/50x 에러 모두 포함)
   then
@@ -26,7 +26,7 @@ function find_idle_profile() {
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port()
 {
-  IDEL_PROFILE=$(find_idle_profile)
+  IDLE_PROFILE=$(find_idle_profile)
 
   if [ ${IDLE_PROFILE} == real1 ]
   then
